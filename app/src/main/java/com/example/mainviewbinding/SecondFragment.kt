c11279cfb43faa25f1da4b647ee3ff85164c2795
Wily13 <wily.355@gmail.com>
2020-11-22 11:24:07 +0700

    add second fragment

--- app/src/main/java/com/example/mainviewbinding/SecondFragment.kt
+++ app/src/main/java/com/example/mainviewbinding/SecondFragment.kt
@@ -5,13 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mainviewbinding.databinding.FragmentFirstBinding
+import com.example.mainviewbinding.databinding.FragmentSecondBinding
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
-    private var _binding : FragmentFirstBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -19,13 +17,10 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater)
         val view = binding.root
 
-        binding.tvFirstFragment.text = "this is view binding in fragment first"
-
         return view
-
     }
 
     override fun onDestroyView() {
